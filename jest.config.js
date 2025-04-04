80388e86d9ede78a908e036b4fe0db0c0e083799
Yves <yves.as@gmail.com>
2025-04-03 23:35:17 -0300

    :test_tube: Tests!

--- jest.config.js
+++ jest.config.js
@@ -9,10 +9,10 @@ module.exports = {
     ],
     coverageThreshold: {
         global: {
-            branches: 80,
-            functions: 80,
-            lines: 80,
-            statements: 80
+            statements: 70,
+            branches: 60,
+            functions: 70,
+            lines: 70
         }
     }
 };
